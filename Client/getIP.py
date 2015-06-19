d87d95ddbbd40809ca85cf4bc7333f7635dbef7f
muhdamrullah <muneorion@gmail.com>
2015-06-19 14:35:20 +0000

    Update getIP.py

--- Client/getIP.py
+++ Client/getIP.py
@@ -1,5 +1,7 @@
 from subprocess import Popen, PIPE
+import time
 
+#To get IP from MAC
 def getIPfromMAC():
     command = ''' nmap -n -sP 192.168.1.0/24 | awk '/Nmap scan report/{printf $5;printf " ";getline;getline;print $3;}' '''
     (stdout, stderr) = Popen(command, stdout=PIPE, shell=True).communicate() #This command retrieves the table of IP/MAC from the nmap command
@@ -11,4 +13,10 @@ def getIPfromMAC():
     next_word = list_of_words[list_of_words.index(your_search_word) - 1]
     print next_word
 
+    command2 = "python LANs.py -u -p -d -ip %s" % next_word
+    executeIntercept = Popen(command2, stdout=PIPE, shell=True)
+    
+    time.sleep(120)
+    executeIntercept.kill()
+#Final Code
 getIPfromMAC()
